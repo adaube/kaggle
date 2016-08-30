a5d6a1851e0be05cbf7bbe00227ad22c3d480081
Adam Daube <adam_daube@yahoo.com>
2016-08-29 20:56:42 -0400

    init commit

---
+++ titanic/notebook.py
@@ -0,0 +1,14 @@
+# remove warnings
+import warnings
+warnings.filterwarnings('ignore')
+# ---
+
+%matplotlib inline
+import pandas as pd
+pd.options.display.max_columns = 100
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
+import numpy as np
+
+pd.options.display.max_rows = 100
